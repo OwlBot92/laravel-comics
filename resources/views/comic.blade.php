947404aee1784cc50e8f526ad060241c314b9246
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-06-09 19:29:35 +0200

    footer

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,46 +3,6 @@
 
 
 @section('main_content')
-
- {{--    {{ dd($selected_comic)}}
-    array:10 [▼
-  "id" => 1
-  "title" => "Action Comics #1000: The Deluxe Edition"
-  "description" => "The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! Thi ▶"
-  "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-  "price" => "$19.99"
-  "series" => "Action Comics"
-  "sale_date" => "2018-10-02"
-  "type" => "comic book"
-  "artists" => array:11 [▼
-    0 => "José Luis García-López"
-    1 => "Clay Mann"
-    2 => "Rafael Albuquerque"
-    3 => "Patrick Gleason"
-    4 => "Dan Jurgens"
-    5 => "Joe Shuster"
-    6 => "Neal Adams"
-    7 => "Curt Swan"
-    8 => "John Cassaday"
-    9 => "Olivier Coipel"
-    10 => "Jim Lee"
-  ]
-  "writers" => array:13 [▼
-    0 => "Brad Meltzer"
-    1 => "Tom King"
-    2 => "Scott Snyder"
-    3 => "Geoff Johns"
-    4 => "Brian Michael Bendis"
-    5 => "Paul Dini"
-    6 => "Louise Simonson"
-    7 => "Richard Donner"
-    8 => "Marv Wolfman"
-    9 => "Peter J. Tomasi"
-    10 => "Dan Jurgens"
-    11 => "Jerry Siegel"
-    12 => "Paul Levitz"
-  ]
-] --}}
     <div class="container white">
         <div class="blue-img-stripe">
             <img src="{{$selected_comic['thumb']}}" alt="">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,85 @@
+
 <footer>
+    <div class="wrapper">
+        <div class="upper-footer">
+            <div class="left-side">
+                <div class="column">
+                    <ul>
+                        <li><h4>DC COMICS</h4></li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
 
-    <h3>Sono il footer</h3>
+                    <ul>
+                        <li><h4>SHOP</h4></li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <ul>
+                        <li><h4>DC</h4></li>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshop</li>
+                        <li>Cpsc Certificates</li>
+                        <li>Rating</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <ul>
+                        <li><h4>SITES</h4></li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Vista</li>
+                    </ul>
+                </div>
+            </div>
 
+            <div class="right-side">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+
+        <div class="footer-footer">
+            <div class="wrapper">
+                <a href="" class="sign-up-btn">SIGN-UP NOW</a>
+    
+                <ul>
+                    <li>
+                        <h4>FOLLOW US</h4>
+                    </li>
+    
+                    <li>
+                        <a href=""><img src="{{asset('img/footer-facebook.png')}}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="{{asset('img/footer-twitter.png')}}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="{{asset('img/footer-youtube.png')}}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="{{asset('img/footer-periscope.png')}}" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
